ddcc09e8d7b37414530e430aba93547a60c24939
Marek <364021318@qq.com>
2024-05-29 11:57:22 +0800

    feat: ants default use groutine pool

---
+++ 2024/0529/ants/default/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/panjf2000/ants/v2"
+)
+
+// 创建一个 包装物 用来包装 groutine 的执行
+func wrapper(i int, wg *sync.WaitGroup) func() {
+	return func() {
+		fmt.Printf("hello from task:%d\n", i)
+		time.Sleep(1 * time.Second)
+		wg.Done()
+	}
+}
+
+func main() {
+	defer ants.Release()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	for i := 1; i <= 2; i++ {
+		ants.Submit(wrapper(i, &wg))
+	}
+
+	wg.Wait()
+}
---
+++ 2024/0529/ants/go.mod
@@ -0,0 +1,5 @@
+module github.com/Marek-maxs/ants
+
+go 1.22.3
+
+require github.com/panjf2000/ants/v2 v2.9.1 // indirect
