786cb2a028c7c189e099454e96118566dcfb3b38
Marek <364021318@qq.com>
2024-06-11 21:43:02 +0800

    feat: loop start and end time when start qt end time

--- 2024/0606/main.go
+++ 2024/0606/main.go
@@ -1,1 +1,30 @@
-package _606
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const oneDaySeconds = 86400
+
+func main() {
+	startDate := int64(1714492800)
+	endDate := int64(1717257600)
+	//povID := "56575f1c-a9fb-4c29-8291-7c86742f73aa"
+	//loc := "Asia/Hong_Kong"
+
+	createAt := startDate
+
+	bQuit := false
+	for !bQuit {
+		startTime := endDate - oneDaySeconds
+		// rabbitmq.PublishEventEnergySummaryChanged(povID, startTime, endDate, timezoneRes.GetTimezone())
+		fmt.Println("startTime:", startTime, "endTime:", endDate)
+		fmt.Println("startDate:", time.Unix(startTime, 0).Format(time.DateTime), "endDate:", time.Unix(endDate, 0).Format(time.DateTime))
+		if startTime < createAt {
+			bQuit = true
+		}
+
+		endDate = startTime
+	}
+}
